67ec8860e2edd7262351ae484bb6916d9158b5a0
Văn Hiền <thovanhien@gmail.com>
2021-02-06 16:50:46 +0700

    dashboard demo

--- src/Layouts/PrivateLayout/PrivateLayout.Component.js
+++ src/Layouts/PrivateLayout/PrivateLayout.Component.js
@@ -1,21 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import {Layout} from "antd"
-import "./PrivateLayout.scss"
+import { Layout, Menu, Breadcrumb } from "antd";
+import "./PrivateLayout.scss";
+import Title from "antd/lib/typography/Title";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
-const {Header, Content, Footer} = Layout
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
-const PrivateLayout = ({children}) => {
-    return (
-        <Layout className="pl-layout">
-            {/* header de tam thoi - sau nay design rieng co them notification & user setting  */}
-            <Header className="pl-layout_header"/>
-            <Content>{children}</Content>
-            <Footer className="pl-layout_footer">
-                {"Travel & Food Social by Pum"} 
-            </Footer>
-        </Layout>
-    )
-}
+const PrivateLayout = ({ children }) => {
+  const [isCollapsed, setCollapsed] = useState(false);
 
-export default PrivateLayout
+  function handleOnCollapse() {
+    const newState = !isCollapsed;
+    console.log(newState);
+    setCollapsed(newState);
+  }
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={isCollapsed} onCollapse={handleOnCollapse}>
+        <div className="logo">
+          <i class="fas fa-image    ">LOGO'S ADMIN</i>
+        </div>
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Quản lý bài viết
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            icon={<UserOutlined />}
+            title="Quản lý thành viên"
+          >
+            <Menu.Item key="3">Admin</Menu.Item>
+            <Menu.Item key="4">User</Menu.Item>
+            <Menu.Item key="5">Some body ???</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header>{children}</Header>
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Quản lý bài viết</Breadcrumb.Item>
+            <Breadcrumb.Item>TITLE BÀI VIẾT</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            NỘI DUNG
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          <Title level={5}>Travel and food design by me !!!</Title>
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default PrivateLayout;
--- src/Pages/Private/Dashboard/Dashboard.Component.js
+++ src/Pages/Private/Dashboard/Dashboard.Component.js
@@ -1,30 +1,24 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "antd";
 
-import React from 'react'
-import {useHistory} from "react-router-dom"
-import { Button} from "antd"
-
-import './Dashboard.scss'
+import "./Dashboard.scss";
 
 //viet login thanh dang function component nhu vay (hook)
 const Dashboard = () => {
-    const history = useHistory()
+  const history = useHistory();
 
-    const handleLogOut = () => {
-        localStorage.setItem("isAuthentication", false);
-        history.push('sign-in')
-    }
-    return (
-        <div className="dbd-container">
-         
-                    <span>Welcome to Dashboard Panel</span>
-                    {" ---- "}
-                    <Button onClick={handleLogOut}>Logout</Button>
-             
-               
-            
-           
-        </div>
-    )
-}
+  const handleLogOut = () => {
+    localStorage.setItem("isAuthentication", false);
+    history.push("sign-in");
+  };
+  return (
+    <div className="dbd-container">
+      <span>Welcome to Dashboard Panel</span>
+      {" ---- "}
+      <Button onClick={handleLogOut}>Logout</Button>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
